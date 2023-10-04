d3649ce2e1cf0a71e650c43351b1a7ec3b394a9d
driko <67830361+drikomak@users.noreply.github.com>
2023-10-04 12:57:18 +0200

    avancement html/css

--- main.php
+++ main.php
@@ -9,14 +9,17 @@
 
 <body>
     <header>
-        <h1>Analitics</h1>
-        <span class = "head-main">
+        
+
         <a href="main.php">Index</a>
-        <a href="pages/page1.php">Page 1</a>
-        <a href="pages/page2.php">Page 2</a>
-        <a href="pages/page2.php">Page 2</a>
+        <span class="head-main">
+        <a href="pages/page1.php" >Page 1</a>
+        <a href="pages/page2.php" >Page 2</a>
+        <a href="pages/page3.php" >Page 3</a>
         </span>
+
     </header>
     
+    <h1 class="main-title">Analitics</h1>
 </body>
 </html>
--- pages/page2.php
+++ pages/page2.php
@@ -7,6 +7,17 @@
     <title>Page 2</title>
 </head>
 <body>
-    
+    <header>
+
+        <a href="../main.php">Index</a>
+        <span class="head-main">
+        
+        <a href="../pages/page1.php" class="head-main">Page 1</a>
+        <a href="../pages/page2.php" class="head-main">Page 2</a>
+        <a href="../pages/page3.php" class="head-main">Page 3</a>
+        </span>
+
+        <h1 class="main-title">Analitics</h1>
+    </header>
 </body>
 </html>
