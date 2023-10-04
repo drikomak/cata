55a24f59b1a36940bf5f7ed94247d8c8a72bfe2b
driko <67830361+drikomak@users.noreply.github.com>
2023-10-04 12:35:26 +0200

    avancement html/css

--- main.php
+++ main.php
@@ -3,12 +3,19 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="styles/styles.css" rel="stylesheet" />
     <title>analitics</title>
 </head>
 
 <body>
     <header>
         <h1>Analitics</h1>
+        <span class = "head-main">
+        <a href="main.php">Index</a>
+        <a href="pages/page1.php">Page 1</a>
+        <a href="pages/page2.php">Page 2</a>
+        <a href="pages/page2.php">Page 2</a>
+        </span>
     </header>
     
 </body>
