3dfa3f4c85d2eeff225e1a6319940ac01bc537d1
driko <67830361+drikomak@users.noreply.github.com>
2024-02-11 20:22:08 +0100

    algo python

---
+++ algo/dl_algo.py
@@ -0,0 +1,11 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+
+
+hurdat2 = pd.read_csv('hurdat2.txt')
+
+def StormCountByCategory(hurdat2):
+  grouped = hurdat2.groupby('Cat')['StormID'].unique().to_frame().reset_index()
+  grouped['Num'] = grouped['StormID'].str.len()
+  grouped.plot.bar(x='Cat', y='Num')
+  plt.ylabel('Number of Storms')
