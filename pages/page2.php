deb0fc8b53ec4a7de5014158214db6aa487a04f9
Ilyas El Boujadaini <elbj.ilyas@gmail.com>
2024-02-11 18:57:43 +0100

    modif css et v1 de la page 2

--- pages/page2.php
+++ pages/page2.php
@@ -1,13 +1,22 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+    <?php require('../bdd.php'); 
+    // Appel de la fonction pour obtenir la connexion à la base de données
+    $bdd = getBD();
+
+    // Exécution de la requête SQL pour récupérer les noms
+    $requete = $bdd->prepare("SELECT DISTINCT name FROM corrected_hurricane_data");
+    $requete->execute();
+    $noms= $requete->fetchAll(PDO::FETCH_COLUMN);
+    ?>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100&display=swap" rel="stylesheet">
-    <link href="../styles/styles.css" rel="stylesheet" />
-    <title>Page 2</title>
+    <link href="../styles/styles.css" rel="stylesheet">
+    <title>Raw Data</title>
 </head>
 <body>
     <header>
@@ -25,5 +34,26 @@
     </nav>
 
     </header>
+    <h1><span class="txt">DONNEES BRUTES <?php echo "Banane";?></span></h1>
+
+    <h2><span class="txt">Statistique : Des données qui concernent l'Amérique du Nord</span></h2>
+    <div class="photo" ><img class="imgp2" src="../images/north_america.jpg" alt="north america map"></div>
+    <p><span class=txt>Pour entrainer notre modèle de prédiction et pour consulter les statistiques des ouragans dans un but éducatif, nous avons récolté des données météorologiques sur les ouragans ayant frappé l'Amérique du nord</span></p>
+    <h3><span class=txt>Tableau des ouragans</span></h3>
+
+    
+    <select name="noms" class="txt">
+    <?php
+    foreach($noms as $nom){
+        echo "<option value='".$nom."'>".$nom."</option>";
+    }
+    ?>
+    </select>
+
+
+<h2><span class=txt>Temps réel : Utilisation d'un API</span></h2>
+<div class=photo><img class="imgp2" src="../images/api.webp" alt=""> <img class="imgp2" src="../images/dataflow.webp" alt=""></div>
+<p><span class=txt>Les données météorologiques sont récoltées en temps réel grâce à l'API OpenWeatherMap. Cet API nous permet de récupérer des données météorologiques sur n'importe quelle ville du monde. Ces données sont ensuite utilisées pour être analysées par notre modèle de prédiction et afficher un résultat.</span></p>
+ 
 </body>
 </html>
