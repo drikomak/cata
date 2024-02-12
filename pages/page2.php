afbef41f110656e0b48db1895d7c09830ae22c2f
Ilyas El Boujadaini <elbj.ilyas@gmail.com>
2024-02-12 19:08:11 +0100

    p2: ajout 2e select et debut de test graph

--- pages/page2.php
+++ pages/page2.php
@@ -6,9 +6,9 @@
     $bdd = getBD();
 
     // Exécution de la requête SQL pour récupérer les noms
-    $requete = $bdd->prepare("SELECT DISTINCT name FROM corrected_hurricane_data");
-    $requete->execute();
-    $noms= $requete->fetchAll(PDO::FETCH_COLUMN);
+    $requeteListeNoms = $bdd->prepare("SELECT DISTINCT name FROM corrected_hurricane_data");
+    $requeteListeNoms->execute();
+    $noms= $requeteListeNoms->fetchAll(PDO::FETCH_COLUMN);
     ?>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -16,14 +16,16 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100&display=swap" rel="stylesheet">
     <link href="../styles/styles.css" rel="stylesheet">
-    <title>Raw Data</title>
+    <title>Données Brutes</title>
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+
 </head>
 <body>
     <header>
 
     <nav class="nav-bar">
     <a href="../main.php"><img class="logo" src="../images/logo3.png"></a>
-        <div class="nav-links">
+        <div class="nav-links">^
             <ul>
                 <li><a href="../pages/page1.php">Algorithm</a></li>
                 <li><a href="../pages/page2.php">Rawdata</a></li>
@@ -38,11 +40,11 @@
 
     <h2><span class="txt">Statistique : Des données qui concernent l'Amérique du Nord</span></h2>
     <div class="photo" ><img class="imgp2" src="../images/north_america.jpg" alt="north america map"></div>
-    <p><span class=txt>Choisissez l'ouragan et le paramètre que vous voulez étudier</span></p>
+    
     <h3><span class=txt>Tableau des ouragans</span></h3>
 
-    
-    <select name="noms" class="txt">
+    <p><span class=txt>Choisissez l'ouragan et le paramètre que vous voulez étudier</span></p>
+    <div class="txt"><select name="noms" class="txt">
     <?php
     foreach($noms as $nom){
         echo "<option value='".$nom."'>".$nom."</option>";
@@ -50,10 +52,22 @@
     ?>
     </select>
 
+    <select name="param" class=txt>
+        <option value="wind">Vent</option>
+        <option value="pressure">Pression</option>
+        <option value="exact_sst_anomaly">Anomalie de température surface</option>
+    </select></div>
+
 
 <h2><span class=txt>Temps réel : Utilisation d'un API</span></h2>
 <div class=photo><img class="imgp2" src="../images/api.webp" alt=""> <img class="imgp2" src="../images/dataflow.webp" alt=""></div>
 <p><span class=txt>Les données météorologiques sont récoltées en temps réel grâce à l'API OpenWeatherMap. Cet API nous permet de récupérer des données météorologiques sur n'importe quelle ville du monde. Ces données sont ensuite utilisées pour être analysées par notre modèle de prédiction et afficher un résultat.</span></p>
  
 </body>
+<script>
+$(document).ready(function() {
+    $("body").css("background", "linear-gradient(to bottom, black, rgb(100,100,100)");
+});
+</script>
+
 </html>
